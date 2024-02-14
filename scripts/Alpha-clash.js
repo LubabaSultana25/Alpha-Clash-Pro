de4ed67c5f5faffcda855c43fdae8aa95b2c9264
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-02-14 22:31:52 +0600

    update

--- scripts/Alpha-clash.js
+++ scripts/Alpha-clash.js
@@ -14,6 +14,10 @@ function continueGame() {
     // step:1 generate a random Alphabet
     const alphabet = getARandomAlphabet();
     console.log('Your random Alphabet', alphabet);
+
+    // set randomly Alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
 }
 
 
