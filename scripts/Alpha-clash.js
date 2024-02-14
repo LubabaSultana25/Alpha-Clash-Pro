36a0526e3adafa3ec184bb23168d88084f7f6cca
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-02-14 20:16:05 +0600

    random Alphabet create

--- scripts/Alpha-clash.js
+++ scripts/Alpha-clash.js
@@ -1,11 +1,24 @@
-function play() {
-    //step-1: hide the home screen. to hide the screen add the class hidden to the home section.
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden');
-    // console.log(homeSection.classList);
+// function play() {
+//     //step-1: hide the home screen. to hide the screen add the class hidden to the home section.
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList);
 
-    // show the playground
-    const playgroundSection= document.getElementById('play-ground');
-    playgroundSection.classList.remove('hidden');
-    // console.log(playgroundSection.classList);
+//     // show the playground
+//     const playgroundSection= document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     // console.log(playgroundSection.classList);
+// }
+
+function continueGame() {
+    // step:1 generate a random Alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your random Alphabet', alphabet);
+}
+
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
